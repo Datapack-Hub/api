2881dd92983d44425b038ad4d6455e7bcf6541b9
Silabear <56885288+Silabear@users.noreply.github.com>
2023-02-22 17:55:10 +0000

    no idea and it probably doesnt work

--- user.py
+++ user.py
@@ -22,31 +22,59 @@ async def get_user(id):
 def me():
     t = request.cookies.get("token")
     if not t:
-        return "Authentication required", 403
+        return "Authentication required", 401
     
     return util.get_user_from_token(request.cookies.get("token"))
 
 @user.route("/<int:id>/projects")
 async def user_projects(id):
     conn = sqlite3.connect(config.db)
-    r = conn.execute(f"select type, author, title, icon, url, description, rowid from projects where author = {id}").fetchall()
-    
-    out = []
-    
-    for item in r:
-        out.append({
-            "type":item[0],
-            "author":item[1],
-            "title":item[2],
-            "icon":item[3],
-            "url":item[4],
-            "description":item[5],
-            "ID":item[6]
-        })
-    
-    conn.close()
+    # Check if user is authenticated
+    t = request.cookies.get("token")
+    if t:
+        if util.get_user_from_token(t)["id"] == id:
+            # Get all submissions
+            r = conn.execute(f"select type, author, title, icon, url, description, rowid from projects where author = {id} and status != 'deleted'").fetchall()
+            
+            # Form array
+            out = []
+            for item in r:
+                out.append({
+                    "type":item[0],
+                    "author":item[1],
+                    "title":item[2],
+                    "icon":item[3],
+                    "url":item[4],
+                    "description":item[5],
+                    "ID":item[6]
+                })
+            
+            conn.close()
+                
+            return {
+                "count":len(out),
+                "result":out
+            }
+    else:
+        # Get all PUBLIC submissions
+        r = conn.execute(f"select type, author, title, icon, url, description, rowid from projects where author = {id} and status == 'live'").fetchall()
         
-    return {
-        "count":len(out),
-        "result":out
-    }
+        # Form array
+        out = []
+        for item in r:
+            out.append({
+                "type":item[0],
+                "author":item[1],
+                "title":item[2],
+                "icon":item[3],
+                "url":item[4],
+                "description":item[5],
+                "ID":item[6]
+            })
+        
+        conn.close()
+            
+        return {
+            "count":len(out),
+            "result":out
+        }
