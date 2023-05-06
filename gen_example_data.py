c8c8f0d70cb623502cd4fa81f2aad50d62538245
boop-the-dog <aidenhperry@gmail.com>
2023-05-06 08:30:58 -0400

    formatted

--- gen_example_data.py
+++ gen_example_data.py
@@ -1,15 +1,17 @@
 import sqlite3
 import config
 
+
 def reset(table: str):
     connection = sqlite3.connect(config.DATA + "data.db")
-    
+
     if table != "no-drop":
         if table == "projects":
             connection.execute(f"DROP TABLE {table}")
 
     # Projects Data
-    connection.execute("""create table IF NOT EXISTS projects(
+    connection.execute(
+        """create table IF NOT EXISTS projects(
         type STRING NOT NULL, 
         author INT NOT NULL, 
         title STRING NOT NULL, 
@@ -23,43 +25,51 @@ def reset(table: str):
         uploaded INT NOT NULL,
         updated INT NOT NULL,
         resource_pack STRING);
-    """)
+    """
+    )
 
     if table == "projects":
-        connection.execute("insert into projects values ('datapack', 1, 'Realistic Item Drops', 'Drops Realsitc short', 'actually very long description', 'https://cdn.discordapp.com/attachments/723984082853298297/1076083669409730590/IMG_2434.png', 'realistic-item-drops', 'live', 0, '[\"utility\"]', 0, 0, 'google.com');")
-
+        connection.execute(
+            "insert into projects values ('datapack', 1, 'Realistic Item Drops', 'Drops Realsitc short', 'actually very long description', 'https://cdn.discordapp.com/attachments/723984082853298297/1076083669409730590/IMG_2434.png', 'realistic-item-drops', 'live', 0, '[\"utility\"]', 0, 0, 'google.com');"
+        )
 
     # User data
-    connection.execute("""create table IF NOT EXISTS users (
+    connection.execute(
+        """create table IF NOT EXISTS users (
         username string NOT NULL UNIQUE, 
         token string NOT NULL UNIQUE, 
         role string NOT NULL, 
         bio STRING, 
         github_id int NOT NULL UNIQUE,
         profile_icon string NOT NULL
-    )""")
+    )"""
+    )
 
     # Banned User Data
-    connection.execute("""create table IF NOT EXISTS banned_users (
+    connection.execute(
+        """create table IF NOT EXISTS banned_users (
         id int NOT NULL UNIQUE,
         expires int,
         reason string
-    )""")
+    )"""
+    )
 
     # Notification Data
-    connection.execute("""create table if not exists notifs(
+    connection.execute(
+        """create table if not exists notifs(
         message STRING NOT NULL,
         description STRING NOT NULL,
         read BOOL NOT NULL,
         user INT NOT NULL,
         type STRING NOT NULL
     );
-    """)
+    """
+    )
 
     # save and exit
     connection.commit()
     connection.close()
-    
+
+
 if __name__ == "__main__":
-    
     reset("no-drop")
--- user.py
+++ user.py
@@ -10,7 +10,7 @@ import json
 
 import util
 
-user = Blueprint("user",__name__,url_prefix="/user")
+user = Blueprint("user", __name__, url_prefix="/user")
 
 # CORS(user,supports_credentials=True)
 
@@ -21,31 +21,35 @@ user = Blueprint("user",__name__,url_prefix="/user")
 #     # Other headers can be added here if needed
 #     return resp
 
+
 @user.route("/staff/<role>")
 def staff(role):
     conn = sqlite3.connect(config.DATA + "data.db")
     if role == "default":
         return "Role has to be staff role", 400
-    list = conn.execute(f"select username, rowid, role, bio, profile_icon from users where role = '{role}'").fetchall()
+    list = conn.execute(
+        f"select username, rowid, role, bio, profile_icon from users where role = '{role}'"
+    ).fetchall()
     finale = []
     for i in list:
-        finale.append({
-            "id":i[1],
-            "username":i[0],
-            "role":i[2],
-            "bio":i[3],
-            "profile_icon":i[4]
-        })
-    return {
-        "count":len(finale),
-        "values":finale
-    }
-    
+        finale.append(
+            {
+                "id": i[1],
+                "username": i[0],
+                "role": i[2],
+                "bio": i[3],
+                "profile_icon": i[4],
+            }
+        )
+    return {"count": len(finale), "values": finale}
+
+
 @user.route("/staff/roles")
 def roles():
     return json.load(open(config.DATA + "roles.json", "r+"))
-    
-@user.route("/<string:username>", methods=["GET","PATCH"])
+
+
+@user.route("/<string:username>", methods=["GET", "PATCH"])
 def get_user(username):
     # TODO mods can see banned users
     u = util.get_user.from_username(username)
@@ -53,7 +57,8 @@ def get_user(username):
         return "User does not exist", 404
     return u
 
-@user.route("/id/<int:id>", methods=["GET","PATCH"])
+
+@user.route("/id/<int:id>", methods=["GET", "PATCH"])
 def get_user_id(id):
     # TODO mods can see banned users
     if request.method == "GET":
@@ -63,71 +68,82 @@ def get_user_id(id):
         return u
     elif request.method == "PATCH":
         dat = request.get_json(force=True)
-        
+
         usr = util.authenticate(request.headers.get("Authorization"))
         if usr == 32:
             return "Please make sure authorization type = Basic"
         if usr == 33:
             return "Token Expired", 498
-        
+
         banned = util.get_user_ban_data(usr["id"])
         if banned is not None:
             return {
-                "banned":True,
-                "reason":banned["reason"],
-                "expires":banned["expires"]
+                "banned": True,
+                "reason": banned["reason"],
+                "expires": banned["expires"],
             }, 403
-        
-        if not (usr["id"] == id or usr["role"] in ["moderator","admin"]):
+
+        if not (usr["id"] == id or usr["role"] in ["moderator", "admin"]):
             return "You aren't allowed to edit this user!", 403
-        
+
         conn = sqlite3.connect(config.DATA + "data.db")
         try:
-            conn.execute(f"UPDATE users SET username = '{dat['username']}' where rowid = {id}")
+            conn.execute(
+                f"UPDATE users SET username = '{dat['username']}' where rowid = {id}"
+            )
             conn.execute(f"UPDATE users SET bio = '{dat['bio']}' where rowid = {id}")
             if usr["role"] == "admin":
-                conn.execute(f"UPDATE users SET role = '{dat['role']}' where rowid = {id}")
-                conn.execute(f"insert into mod_logs values ({usr['id']}, '{usr['username']}', 'Edited user {dat['id']}',{int( time.time() )})")
-                util.post_site_log(usr["username"],"Edited user",f"Edited user data of {dat['username']}")
-        except sqlite3.Error as er: 
+                conn.execute(
+                    f"UPDATE users SET role = '{dat['role']}' where rowid = {id}"
+                )
+                conn.execute(
+                    f"insert into mod_logs values ({usr['id']}, '{usr['username']}', 'Edited user {dat['id']}',{int( time.time() )})"
+                )
+                util.post_site_log(
+                    usr["username"],
+                    "Edited user",
+                    f"Edited user data of {dat['username']}",
+                )
+        except sqlite3.Error as er:
             return er, 400
         conn.commit()
         conn.close()
         return util.get_user.from_id(id)
 
+
 @user.route("/me")
 def me():
-    #TODO user can see if they are banned
+    # TODO user can see if they are banned
     if not request.headers.get("Authorization"):
         return "Authorization required", 401
-    
+
     usr = util.authenticate(request.headers.get("Authorization"))
-    
+
     if usr == 32:
         return "Please make sure authorization type = Basic"
-    
+
     if usr == 33:
         return "Token Expired", 498
 
-    # banned?
+    # banned?
     conn = sqlite3.connect(config.DATA + "data.db")
-    x = conn.execute("SELECT rowid, expires, reason from banned_users where id = "+ str(usr["id"])).fetchall()
+    x = conn.execute(
+        "SELECT rowid, expires, reason from banned_users where id = " + str(usr["id"])
+    ).fetchall()
     if len(x) == 1:
-        current = int( time.time() )
+        current = int(time.time())
         expires = x[0][1]
         if current > expires:
             conn.execute(f"delete from banned_users where rowid = {str(x[0][0])}")
             conn.commit()
         else:
             usr["banned"] = True
-            usr["banData"] = {
-                "message":x[0][2],
-                "expires":expires
-            }
+            usr["banData"] = {"message": x[0][2], "expires": expires}
     else:
         usr["banned"] = False
     conn.close()
     return usr
+
 
 @user.route("/<string:username>/projects")
 def user_projects(username):
@@ -135,83 +151,86 @@ def user_projects(username):
     # Check if user is authenticated
     t = request.headers.get("Authorization")
     user = util.get_user.from_username(username)
-    
+
     authed = util.authenticate(t)
-    
+
     if authed == 32:
         return "Make sure authorization is basic!", 400
     elif authed == 33:
-        return "Token expired!",429
-    
+        return "Token expired!", 429
+
     if t:
         if authed["id"] == user["id"]:
             # Get all submissions
-            r = conn.execute(f"select type, author, title, icon, url, description, rowid, status from projects where author = {user['id']} and status != 'deleted'").fetchall()
-            
+            r = conn.execute(
+                f"select type, author, title, icon, url, description, rowid, status from projects where author = {user['id']} and status != 'deleted'"
+            ).fetchall()
+
             # Form array
             out = []
             for item in r:
-                out.append({
-                    "type":item[0],
-                    "author":item[1],
-                    "title":item[2],
-                    "icon":item[3],
-                    "url":item[4],
-                    "description":item[5],
-                    "ID":item[6],
-                    "status":item[7]
-                })
-            
+                out.append(
+                    {
+                        "type": item[0],
+                        "author": item[1],
+                        "title": item[2],
+                        "icon": item[3],
+                        "url": item[4],
+                        "description": item[5],
+                        "ID": item[6],
+                        "status": item[7],
+                    }
+                )
+
             conn.close()
-                
-            return {
-                "count":len(out),
-                "result":out
-            }
+
+            return {"count": len(out), "result": out}
         else:
             # Get all PUBLIC submissions
-            r = conn.execute(f"select type, author, title, icon, url, description, rowid, status from projects where author = {user['id']} and status == 'live'").fetchall()
-            
+            r = conn.execute(
+                f"select type, author, title, icon, url, description, rowid, status from projects where author = {user['id']} and status == 'live'"
+            ).fetchall()
+
             # Form array
             out = []
             for item in r:
-                out.append({
-                    "type":item[0],
-                    "author":item[1],
-                    "title":item[2],
-                    "icon":item[3],
-                    "url":item[4],
-                    "description":item[5],
-                    "ID":item[6],
-                    "status":item[7]
-                })
-            
+                out.append(
+                    {
+                        "type": item[0],
+                        "author": item[1],
+                        "title": item[2],
+                        "icon": item[3],
+                        "url": item[4],
+                        "description": item[5],
+                        "ID": item[6],
+                        "status": item[7],
+                    }
+                )
+
             conn.close()
-                
-            return {
-                "count":len(out),
-                "result":out
-            }
+
+            return {"count": len(out), "result": out}
     else:
         # Get all PUBLIC submissions
-        r = conn.execute(f"select type, author, title, icon, url, description, rowid from projects where author = {user['id']} and status == 'live'").fetchall()
-        
+        r = conn.execute(
+            f"select type, author, title, icon, url, description, rowid from projects where author = {user['id']} and status == 'live'"
+        ).fetchall()
+
         # Form array
         out = []
         for item in r:
-            out.append({
-                "type":item[0],
-                "author":item[1],
-                "title":item[2],
-                "icon":item[3],
-                "url":item[4],
-                "description":item[5],
-                "ID":item[6]
-            })
-        
+            out.append(
+                {
+                    "type": item[0],
+                    "author": item[1],
+                    "title": item[2],
+                    "icon": item[3],
+                    "url": item[4],
+                    "description": item[5],
+                    "ID": item[6],
+                }
+            )
+
         conn.close()
-            
-        return {
-            "count":len(out),
-            "result":out
-        }
+
+        return {"count": len(out), "result": out}
