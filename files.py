be2ef45b183d9c56593ad051d00709e59f02e966
Silabear <justsilas@yahoo.co.uk>
2023-05-09 17:06:04 +0100

    file uploadage

---
+++ files.py
@@ -0,0 +1,20 @@
+"""
+This code does some weird ass stuff which should probably upload files to the cloudflare... dont ask me
+"""
+
+import requests
+import config
+
+def upload_file(file, file_name:str, uploader:str):
+    put = requests.put("https://files.datapackhub.net/" + file_name, file, headers={
+        "Authorization":config.FILES_TOKEN,
+        "Author":uploader
+    })
+    if put.ok:
+        return "https://files.datapackhub.net/" + file_name
+    else:
+        print(put.text)
+        return False
+    
+if __name__ == "__main__":
+    upload_file(open("D:\Datapack Hub testing zips\Datapack.zip","rb"),"Datapack.zip","Silabear")
