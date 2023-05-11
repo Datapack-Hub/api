d63e871358c644bda31fe1e4eafcfa989281b651
boop-the-dog <aidenhperry@gmail.com>
2023-05-11 13:18:12 -0400

    yummy clean up and linting :)

--- versions.py
+++ versions.py
@@ -7,7 +7,6 @@ from flask import Blueprint, request
 import util
 import sqlite3
 import config
-import regex as re
 import files
 
 versions = Blueprint("versions", __name__, url_prefix="/versions")
@@ -34,18 +33,18 @@ def project(id: int):
             o["resource_pack_download"]: i[3]
 
         out.append(o)
-        
-    return {
-        "count":len(out),
-        "result":out
-    }
-    
+
+    return {"count": len(out), "result": out}
+
+
 @versions.route("/project/<int:id>/<string:code>")
-def code(id:int, code: str):
+def code(id: int, code: str):
     # Select all versions where the project is this one
     conn = sqlite3.connect(f"{config.DATA}data.db")
-    v = conn.execute(f"SELECT * FROM versions WHERE version_code = '{code}' AND project = {id}").fetchone()
-    
+    v = conn.execute(
+        f"SELECT * FROM versions WHERE version_code = '{code}' AND project = {id}"
+    ).fetchone()
+
     return {
         "name": v[0],
         "description": v[1],
@@ -81,28 +80,45 @@ def new(project: int):
     # now do the stuff
     data = request.get_json(force=True)
     conn = sqlite3.connect(f"{config.DATA}data.db")
-    
+
     try:
-        data['name']
-        data['description']
-        data['minecraft_versions']
-        data['version_code']
+        data["name"]
+        data["description"]
+        data["minecraft_versions"]
+        data["version_code"]
         data["primary_download"]
         data["filename"]
     except:
-        return "Make sure you provide name, description, minecraft_versions, version_code, primary_download, filename and optionally resource_pack_download",400
+        return (
+            "Make sure you provide name, description, minecraft_versions, version_code, primary_download, filename and optionally resource_pack_download",
+            400,
+        )
     else:
-        dpath = files.upload_file(data["primary_download"],f"project/{project}/{data['version_code']}/{data['filename']}",usr["username"])
+        dpath = files.upload_file(
+            data["primary_download"],
+            f"project/{project}/{data['version_code']}/{data['filename']}",
+            usr["username"],
+        )
         try:
             data["resource_pack_download"]
         except:
-            conn.execute(f"INSERT INTO versions(name,description,primary_download,minecraft_versions,version_code,project) VALUES ('{data['name']}', '{data['description']}', '{dpath}','{','.join(data['minecraft_versions'])}', '{data['version_code']}', {str(project)})")
+            conn.execute(
+                f"INSERT INTO versions(name,description,primary_download,minecraft_versions,version_code,project) VALUES ('{data['name']}', '{data['description']}', '{dpath}','{','.join(data['minecraft_versions'])}', '{data['version_code']}', {str(project)})"
+            )
         else:
-            rpath = files.upload_file(data["resource_pack_download"],f"project/{project}/{data['version_code']}/{data['filename']}",usr["username"])
-            conn.execute(f"INSERT INTO versions(name,description,primary_download,resource_pack_download,minecraft_versions,version_code,project) VALUES ('{data['name']}', '{data['description']}', '{dpath}','{rpath}','{','.join(data['minecraft_versions'])}', '{data['version_code']}', {str(project)})")
-    
-    v = conn.execute(f"SELECT * FROM versions WHERE version_code = '{data['version_code']}'").fetchone()
-    
+            rpath = files.upload_file(
+                data["resource_pack_download"],
+                f"project/{project}/{data['version_code']}/{data['filename']}",
+                usr["username"],
+            )
+            conn.execute(
+                f"INSERT INTO versions(name,description,primary_download,resource_pack_download,minecraft_versions,version_code,project) VALUES ('{data['name']}', '{data['description']}', '{dpath}','{rpath}','{','.join(data['minecraft_versions'])}', '{data['version_code']}', {str(project)})"
+            )
+
+    v = conn.execute(
+        f"SELECT * FROM versions WHERE version_code = '{data['version_code']}'"
+    ).fetchone()
+
     o = {
         "name": v[0],
         "description": v[1],
@@ -113,7 +129,7 @@ def new(project: int):
 
     if v[3] is not None:
         o["resource_pack_download"]: v[3]
-        
+
     conn.commit()
     conn.close()
 
