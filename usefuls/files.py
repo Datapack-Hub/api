b3e6f97a3ac907a12e664631de907b13b77cd1d6
Silabear <justsilas@yahoo.co.uk>
2023-06-15 14:52:36 +0100

    that moment when you commit bug to prod

--- usefuls/files.py
+++ usefuls/files.py
@@ -37,7 +37,7 @@ def upload_zipfile(file: str, file_name: str, uploader: str, squash: bool = Fals
 
 def upload_file(file: str, file_name: str, uploader: str):
     file = file.encode("unicode_escape")
-    decoded = base64.b64decode(file[41:])
+    decoded = base64.b64decode(file.split(",")[1])
 
     with open(config.DATA + "tempfile", "wb") as out:
         out.write(decoded)
