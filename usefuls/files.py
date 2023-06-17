6be3c2472bae0f47d10df94fe5a72a76886de786
Silabear <56885288+Silabear@users.noreply.github.com>
2023-06-17 11:35:35 +0100

    Update files.py

--- usefuls/files.py
+++ usefuls/files.py
@@ -10,7 +10,7 @@ import os
 
 
 def upload_zipfile(file: str, file_name: str, uploader: str, squash: bool = False):
-    print(file.split(",")[0])
+    print(file.split(",")[1])
     file = file.split(",")[1]
     file = file.encode("unicode_escape")
     decoded = base64.b64decode(file)
