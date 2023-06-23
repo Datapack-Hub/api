4b17c7b76089b65f8ad213dfab829d3441002f15
Silabear <justsilas@yahoo.co.uk>
2023-06-23 15:56:46 +0100

    hmmmmmmmmmmmm

--- usefuls/util.py
+++ usefuls/util.py
@@ -256,7 +256,7 @@ def user_owns_project(project: int, author: int):
 
 
 def sanitise(query: str):
-    return query.replace("'", "''").replace("%", "").replace(";", r"\;")
+    return query.replace("'", r"\'").replace(";", r"\;")
 
 
 if __name__ == "__main__":
