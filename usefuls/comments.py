6a772dc3cdf78127ecad877fdd224bbdeba720f9
boop-the-dog <aidenhperry@gmail.com>
2023-07-07 10:32:52 -0400

    comments stuff, plz fix eventually

--- usefuls/comments.py
+++ usefuls/comments.py
@@ -66,44 +66,12 @@ class CommentTree:
 
     def show_tree(self) -> None:
         """Prints tree for debugging"""
-        self.comment_tree.show()
-
-    def save(self):
-        for comment in self.comment_tree.all_nodes():
-            with sql_engine.connect() as conn:
-                comment_exists = conn.execute(
-                    text("SELECT * FROM comments WHERE id = :id"),
-                    {"id": comment.data.id},
-                ).first()
-
-                if comment_exists is None:
-                    conn.execute(
-                        text(
-                            "INSERT INTO comments (id, author, content, replies) VALUES (:id, :author, :content, :replies)"
-                        ),
-                        vars(comment.data),
-                    )
-                    conn.commit()
-                else:
-                    conn.execute(
-                        text(
-                            "UPDATE comments SET id = :id, author = :author, content = :content, replies = :replies WHERE id = :id"
-                        ),
-                        vars(comment.data),
-                    )
-                    conn.commit()
-                    pass
-                pass
-            pass
-        pass
-
-    pass
-
-
-def load_tree():
-    with sql_engine.connect() as conn:
-        res = conn.execute(text("SELECT * FROM comments ORDER BY id DESC"))
-        print(res.all())
+        print(self.comment_tree.nodes)
+        
+    def to_json(self) -> str:
+        for n in self.comment_tree.all_nodes():
+            print(n.data)
+        
 
 
 if __name__ == "__main__":
@@ -112,11 +80,7 @@ if __name__ == "__main__":
     tree = CommentTree(Comment(1, 1, "Hello World", 1))
 
     tree.add_comment(comment=Comment(2, 1, "Hello World", 1))
-    reply_node_test = tree.add_comment(comment=Comment(3, 1, "Hello World", 1))
-    tree.add_comment(parent=reply_node_test, comment=Comment(4, 1, "Hello World", 3))
+    reply_node_test = tree.add_comment(comment=Comment(3, 1, "Hello World", 4))
+    tree.add_comment(parent=reply_node_test, comment=Comment(4, 1, "Hello World", 0))
 
-    tree.show_tree()
-
-    tree.save()
-
-    load_tree()
+    tree.to_json()
