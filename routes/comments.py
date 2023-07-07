5495c1536373b9d1402088612ebea4b135b274fd
Silabear <justsilas@yahoo.co.uk>
2023-07-07 17:12:04 +0100

    order by sent desc

--- routes/comments.py
+++ routes/comments.py
@@ -15,14 +15,14 @@ comments = Blueprint("comments", __name__, url_prefix="/comments")
 def messages_from_thread(thread: int):
     conn = sqlite3.connect(config.DATA + "data.db")
     cmts = conn.execute(
-        f"select rowid, message, author, sent from comments where thread_id = {thread} and parent_id is null"
+        f"select rowid, message, author, sent from comments where thread_id = {thread} and parent_id is null order by sent desc"
     ).fetchall()
 
     out = []
     for cmt in cmts:
         author = util.get_user.from_id(cmt[2])
         replies = conn.execute(
-            f"select rowid, message, author, sent from comments where thread_id = {thread} and parent_id = {cmt[0]}"
+            f"select rowid, message, author, sent from comments where thread_id = {thread} and parent_id = {cmt[0]} order by sent desc"
         ).fetchall()
         reps = []
         for reply in replies:
