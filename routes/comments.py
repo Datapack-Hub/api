84c231b67e7ababf204fabe6a882411c4ec880bb
Silabear <Silabear@users.noreply.github.com>
2023-07-07 14:49:47 +0000

    Apply formatting

--- routes/comments.py
+++ routes/comments.py
@@ -8,6 +8,7 @@ import config
 
 comments = Blueprint("comments", __name__, url_prefix="/comments")
 
+
 @comments.route("/thread/<int:thread>")
 def messages_from_thread(thread: int):
     conn = sqlite3.connect(config.DATA + "data.db")
@@ -22,5 +23,20 @@ def messages_from_thread(thread: int):
         ).fetchall()
         reps = []
         for reply in replies:
-            reps.append({"id": reply[0], "message": reply[1], "author": reply[2], "sent": reply[4]})
-        out.append({"id": cmt[0], "message": cmt[1], "author": cmt[2], "sent": cmt[4], "replies":reps})
+            reps.append(
+                {
+                    "id": reply[0],
+                    "message": reply[1],
+                    "author": reply[2],
+                    "sent": reply[4],
+                }
+            )
+        out.append(
+            {
+                "id": cmt[0],
+                "message": cmt[1],
+                "author": cmt[2],
+                "sent": cmt[4],
+                "replies": reps,
+            }
+        )
