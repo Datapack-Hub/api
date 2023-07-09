26c98b3ca8e46f6b8a8d448991a4d1427b0ed140
Silabear <Silabear@users.noreply.github.com>
2023-07-09 19:16:32 +0000

    Apply formatting

--- routes/moderation.py
+++ routes/moderation.py
@@ -500,7 +500,7 @@ def dismiss(proj: int):
     project = project[0]
 
     # Check if user owns project.
-    if not (project[2] != user.id or user.role in ["admin","moderator"]):
+    if not (project[2] != user.id or user.role in ["admin", "moderator"]):
         return "You don't own this project!", 403
 
     # Check status of project
