0fc7f32b31f6ea4c95299eafab2982f864c8f92e
HoodieRocks <HoodieRocks@users.noreply.github.com>
2023-09-11 21:29:00 +0000

    Apply formatting

--- utilities/files.py
+++ utilities/files.py
@@ -4,8 +4,9 @@ This code does some weird ass stuff which should probably upload files to the cl
 import config
 
 from PIL import Image
+
 # ignore because pillow is weird
-import pillow_avif # noqa: F401
+import pillow_avif  # noqa: F401
 
 import base64
 import shutil
