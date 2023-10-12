622af4a7ea633562652207ad4699153204198584
Aiden Perry <aidenhperry@gmail.com>
2023-10-11 20:32:10 -0400

    more db lock?

--- routes/projects.py
+++ routes/projects.py
@@ -590,7 +590,7 @@ def publish(id):
         conn.close()
         utilities.post.in_queue(proj[2], proj[3], proj[4], proj[0], proj[5])
         return "The project is now in the publish queue.", 200
-    elif proj[1] == "draft":
+    elif proj[1] == "draft": # why this?
         util.exec_query(
             conn, "update projects set status = 'live' where rowid = :id", id=id
         )
@@ -608,6 +608,7 @@ def publish(id):
         utilities.post.in_queue(proj[2], proj[3], proj[4], proj[0], proj[5])
         return "The project is now in the review queue.", 200
     else:
+        conn.close()
         return "This project is not in a valid state to be published!", 400
 
 
